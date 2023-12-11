6da27edf76c6982d924bc28c385b08c5c40a3af2
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-11 17:58:02 +0530

    renaming

--- packages/astro-font/utils.ts
+++ packages/astro-font/utils.ts
@@ -4,36 +4,36 @@ import { openSync, create } from 'fontkit'
 import { getFallbackMetricsFromFontFile } from './font'
 import { pickFontFileForFallbackGeneration } from './fallback'
 
-async function getFallbackFont(url) {
-  const urls: any[] = []
-  for (let i of url.src) {
+async function getFallbackFont(fontCollection) {
+  const fonts: any[] = []
+  for (let i of fontCollection.src) {
     if (i.path.includes('https://')) {
       const tmp = await fetch(i.path)
       const resp = Buffer.from(await tmp.arrayBuffer())
-      urls.push({
+      fonts.push({
         style: i.style,
         weight: i.weight,
         metadata: create(resp)
       })
     }
     else {
-      urls.push({
+      fonts.push({
         style: i.style,
         weight: i.weight,
         metadata: openSync(i.path)
       })
     }
   }
-  const { metadata } = pickFontFileForFallbackGeneration(urls);
-  return getFallbackMetricsFromFontFile(metadata, url.fallback);
+  const { metadata } = pickFontFileForFallbackGeneration(fonts);
+  return getFallbackMetricsFromFontFile(metadata, fontCollection.fallback);
 }
 
-export function createBaseCSS(url): string {
-  return url.src.map(i => `@font-face{font-style: ${i.style}; font-weight: ${i.weight}; font-display: ${url.display}; font-family: ${url.name}; src: url(/${relative(url.basePath, i.path)});}`)
+export function createBaseCSS(fontCollection): string {
+  return fontCollection.src.map(i => `@font-face{font-style: ${i.style}; font-weight: ${i.weight}; font-display: ${fontCollection.display}; font-family: ${fontCollection.name}; src: url(/${relative(fontCollection.basePath, i.path)});}`)
 }
 
-export async function createFontCSS(url): Promise<string> {
+export async function createFontCSS(fontCollection): Promise<string> {
   const fallbackName = '_font_fallback_' + new Date().getTime()
-  const fallbackFont = await getFallbackFont(url)
-  return `${url.selector}{font-family: ${url.name}, ${fallbackName}, ${url.fallback};}@font-face{font-family: ${fallbackName}; size-adjust: ${fallbackFont.sizeAdjust}; src: local('${fallbackFont.fallbackFont}'); ascent-override: ${fallbackFont.ascentOverride}; descent-override: ${fallbackFont.descentOverride}; line-gap-override: ${fallbackFont.lineGapOverride};}`
+  const fallbackFont = await getFallbackFont(fontCollection)
+  return `${fontCollection.selector}{font-family: ${fontCollection.name}, ${fallbackName}, ${fontCollection.fallback};}@font-face{font-family: ${fallbackName}; size-adjust: ${fallbackFont.sizeAdjust}; src: local('${fallbackFont.fallbackFont}'); ascent-override: ${fallbackFont.ascentOverride}; descent-override: ${fallbackFont.descentOverride}; line-gap-override: ${fallbackFont.lineGapOverride};}`
 }
